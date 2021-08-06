e6af6565ee4eadc5cb0d6410e038078b65514774
Nguyennath <82819781+Nguyennath@users.noreply.github.com>
2021-08-06 19:42:10 +1000

    Add files via upload
    1st version of website i have code pen link too that i will include the comment 
    https://codepen.io/nguyennath/pen/QWvmMMy

---
+++ home.cs
@@ -0,0 +1,33 @@
+header {
+ font-Cambo: 'custom';
+ font-size: 64px;
+ font-weight: bold;
+ text-align: center;
+ letter-spacing: 6px;
+}
+h1 {
+    font-Cambo: 'custom';
+    font-size: 18px;
+    font-weight: bold;
+    text-align: center;
+   }
+
+h2 {
+    font-Cambo: 'custom';
+    font-size: 30px;
+    font-weight: bold;
+    text-align: center;
+    letter-spacing: 6px;
+   }
+
+h3 {
+    font-Cambo: 'custom';
+    font-size: 14px;
+    font-weight: bold;
+    text-align: center;
+   }
+
+table.center {
+    margin-left:auto; 
+    margin-right:auto;
+  }
